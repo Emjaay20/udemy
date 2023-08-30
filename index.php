dcc853d1f8292ead23fbfaa003c876680aabbff8
Yusuf Abubakar Saka <youngmj2010@gmail.com>
2023-08-30 10:40:07 +0100

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <?php wp_head(); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body <?php body_class('example'); ?>>
+
+  <p> Tst</p>
+
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
